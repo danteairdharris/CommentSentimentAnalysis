0a4afd06cd62a71957fbf9bac0b0b513206bd2c5
Dante <danteairdharris@gmail.com>
2021-07-05 19:19:05 -0400

    fixed config with theme

--- setup.sh
+++ setup.sh
@@ -6,4 +6,9 @@ port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
+[theme]\n\
+base="dark"\n\
+primaryColor="#4833f6"\n\
+backgroundColor="#000000"\n\
+\n\
 " > ~/.streamlit/config.toml
